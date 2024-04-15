8229bf6c2c3ae54c819c3d8f06aced307dbee01e
ashiktk85 <111106558+ashiktk85@users.noreply.github.com>
2024-04-15 19:58:14 +0530

    bug

--- database/connection.js
+++ database/connection.js
@@ -6,6 +6,7 @@ mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
     try {
+        // mongoose.connect("mongodb://localhost:27017/kevinhills")
         const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Database successfully connected : ${conn.connection.host}`);
     } catch (error) {
