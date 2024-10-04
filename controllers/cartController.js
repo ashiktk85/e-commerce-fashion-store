8d94271a02d6ea8adc29128aa23f1cd604d61c34
Ashik TK <111106558+ashiktk85@users.noreply.github.com>
2024-10-04 10:38:13 +0530

    ready for deploying

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -16,8 +16,8 @@ const Razorpay = require("razorpay");
 //RAZOR PAY FUNCTION
 
 var instance = new Razorpay({
-  key_id: "rzp_test_ECB3Zjd1NyXFF9",
-  key_secret: "iEgLaQf47PbHbiIodSNL8NvV",
+  key_id: "rzp_test_7PE24PnF4GNlR0",
+  key_secret: "iv9QLiJw3LulPaAAdrpLkGtq",
 });
 
 // LOADING CART PAGE
--- controllers/orderController.js
+++ controllers/orderController.js
@@ -15,8 +15,8 @@ const { generateOrder, otpGnerator } = require("./otpGenerator");
 //RAZOR PAY FUNCTION
 
 var instance = new Razorpay({
-  key_id: "rzp_test_ECB3Zjd1NyXFF9",
-  key_secret: "iEgLaQf47PbHbiIodSNL8NvV",
+  key_id: "rzp_test_7PE24PnF4GNlR0",
+  key_secret: "iv9QLiJw3LulPaAAdrpLkGtq",
 });
 
 // LOAD ADMIN ORDER PAGE
